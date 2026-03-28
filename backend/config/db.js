24a4fb62edf358d25b1375231b095df09a254d8d
Sujal1444 <sujalp875@gmail.com>
2026-03-28 13:35:54 +0530

    c

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,13 +2,13 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/GiftSutra');
-    // const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://Darshan:Versatile@darshanversatile.zcu55.mongodb.net/GiftSutra');
+    // const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/GiftSutra');
+    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://sujalversatileit_db_user:9y5IflR2UYlxRALi@giftsutra.l22qjh2.mongodb.net/GiftSutra');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 };
-
+// 9y5IflR2UYlxRALi
 export default connectDB;
