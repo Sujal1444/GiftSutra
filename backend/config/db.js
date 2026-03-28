c92a5da2a814cf6f6155b50bfa912b7e63bdc8d3
Sujal1444 <sujalp875@gmail.com>
2026-03-28 12:10:41 +0530

    c

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    // const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/GiftSutra');
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://Darshan:Versatile@darshanversatile.zcu55.mongodb.net/GiftSutra');
+    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/GiftSutra');
+    // const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://Darshan:Versatile@darshanversatile.zcu55.mongodb.net/GiftSutra');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
